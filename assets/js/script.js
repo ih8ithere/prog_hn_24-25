2d2f9b983e21875e9b96f6a491209999a599876c
ih8ithere <vincent.lacoste@sorbonne-nouvelle.fr>
2025-03-17 00:42:27 +0100

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,14 @@ document.getElementById("helloButton").addEventListener("click", function() {
 document.getElementById("helpButton").addEventListener("click", function() {
     alert("Il faut entrer son nom et cliquer sur le bouton. Ce n'est pas très compliqué, non? Par exemple, vous vous appellez 'Théodulphe', et bien ça affichera 'Bonjour Théodulphe !'.");
 });
+function exercice4() {
+    let texte = document.getElementById("texteExercice4").value;
+    let tokens = texte.split(" ");
+    let table = document.createElement("table");
+    tokens.forEach(mot => {
+        let row = document.createElement("tr");
+        row.innerHTML = mot;
+        table.appendChild(row);
+    });
+    document.getElementById("exercice4Resultat").appendChild(table);
+}
